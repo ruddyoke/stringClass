f6d20e79142e248401142a6652a10ac3b2bc042f
ruddyoke <lruddy33@gmail.com>
2018-03-21 15:33:37 +0100

    add wd_remove_accents method

--- src/String.php
+++ src/String.php
@@ -112,6 +112,17 @@ class String extends Builder
 	{
 		return mb_convert_encoding($this->string, "UTF-8",$from_encoding);
 	}
+	
+	function wd_remove_accents($str, $charset='utf-8')
+	{
+		$str = htmlentities($str, ENT_NOQUOTES, $charset);
+
+		$str = preg_replace('#&([A-za-z])(?:acute|cedil|caron|circ|grave|orn|ring|slash|th|tilde|uml);#', '\1', $str);
+		$str = preg_replace('#&([A-za-z]{2})(?:lig);#', '\1', $str); // pour les ligatures e.g. '&oelig;'
+		$str = preg_replace('#&[^;]+;#', '', $str); // supprime les autres caractères
+
+		return $str;
+	}
 
 
 }
