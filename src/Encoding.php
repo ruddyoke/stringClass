f78e56b00fa694587e74123d4ea870e446ee9e9f
ruddyoke <lruddy33@gmail.com>
2019-01-15 10:40:30 +0100

    Update Encoding.php

--- src/Encoding.php
+++ src/Encoding.php
@@ -5,6 +5,30 @@ namespace App\Tools;
 
 class Encoding {
 
+	static function getEncoding($filename)
+	{
+		$encoding='';
+		$handle = fopen($filename, 'r');
+		$bom = fread($handle, 2);
+		//  fclose($handle);
+		rewind($handle);
+
+		if($bom === chr(0xff).chr(0xfe)  || $bom === chr(0xfe).chr(0xff)){
+			// UTF16 Byte Order Mark present
+			$encoding = 'UTF-16';
+		} else {
+			$file_sample = fread($handle, 1000) /*+ 'e'*/; //read first 1000 bytes
+			// + e is a workaround for mb_string bug
+			rewind($handle);
+			//$encoding = mb_detect_encoding($file_sample, "auto");
+			$encoding = mb_detect_encoding($file_sample , 'UTF-8, ISO-8859-1, UTF-7, ASCII, EUC-JP,SJIS, eucJP-win, SJIS-win, JIS, ISO-2022-JP');
+		}
+		/*if ($encoding){
+			stream_filter_append($handle, 'convert.iconv.'.$encoding.'/UTF-8');
+		}*/
+		return $encoding;
+	}
+
 	static function windowEncodingToUtf8($string, $from_encoding="ISO-8859-1")
 	{
 		return mb_convert_encoding($string, "UTF-8",$from_encoding);
